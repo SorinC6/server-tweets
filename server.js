e43e2f78d35617a3117be069e92246846bac2121
Sorin Chis <kish.sorin@yahoo.com>
2020-03-28 15:33:49 +0200

    test

--- server.js
+++ server.js
@@ -34,50 +34,59 @@ const deleteImage = () => {
 
 server.post("/imagetotweet", async (req, res) => {
   const { dataUrl } = req.body;
+  // const imageData = null;
   console.log(dataUrl);
   deleteImage();
-  ba64.writeImageSync("./media/myimage", dataUrl);
-  // if (err) {
-  //   console.log(err);
-  //   res.status(500).json({ error: "To many tweets, try again later" });
-  // }
-  console.log("Image saved successfully");
-
-  // do stuff
-  const imageData = fs.readFileSync("./media/myimage.png");
-  try {
-    client.post(
-      "media/upload",
-      {
-        media: imageData
-      },
-      function(error, media, response) {
-        if (error) {
-          console.log(error);
-        } else {
-          const status = {
-            status: "I tweeted from Node.js!",
-            media_ids: media.media_id_string
-          };
-          client.post("statuses/update", status, function(error, response) {
+  ba64.writeImage("./media/myimage", dataUrl, err => {
+    if (err) {
+      console.log("@@@@@@ Nooooo");
+    }
+    console.log("Image saved successfully");
+    fs.readFile("./media/myimage.png", (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+      try {
+        client.post(
+          "media/upload",
+          {
+            media: data
+          },
+          function(error, media, response) {
             if (error) {
               console.log(error);
             } else {
-              debugger;
-              res.status(200).json({
-                message: response.entities.media[0].display_url
+              const status = {
+                status: "I tweeted from Node.js!",
+                media_ids: media.media_id_string
+              };
+              client.post("statuses/update", status, function(error, response) {
+                if (error) {
+                  console.log(error);
+                } else {
+                  debugger;
+                  res.status(200).json({
+                    message: response.entities.media[0].display_url
+                  });
+                  // console.log("Media", response.entities.media[0].display_url);
+                }
               });
-              // console.log("Media", response.entities.media[0].display_url);
             }
-          });
-        }
+          }
+        );
+      } catch (error) {
+        res.status(500).json({ error: error.message });
       }
-    );
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-  deleteImage();
+      deleteImage();
+    });
+  });
 });
+// if (err) {
+//   console.log(err);
+//   res.status(500).json({ error: "To many tweets, try again later" });
+// }
+
+// do stuff
 
 server.get("/img", (req, res) => {
   res.send("<h1>test img</h1>");
