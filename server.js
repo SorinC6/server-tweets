783ef871d4e493698d4d35d3097ababc5f03ee83
Sorin Chis <kish.sorin@yahoo.com>
2020-03-28 15:04:04 +0200

    test

--- server.js
+++ server.js
@@ -36,48 +36,47 @@ server.post("/imagetotweet", async (req, res) => {
   const { dataUrl } = req.body;
   console.log(dataUrl);
   deleteImage();
-  ba64.writeImage("./media/myimage", dataUrl, function(err, i) {
-    if (err) {
-      console.log(err);
-      res.status(500).json({ error: "To many tweets, try again later" });
-    }
-    console.log("Image saved successfully");
+  ba64.writeImageSync("./media/myimage", dataUrl);
+  // if (err) {
+  //   console.log(err);
+  //   res.status(500).json({ error: "To many tweets, try again later" });
+  // }
+  console.log("Image saved successfully");
 
-    // do stuff
-    const imageData = fs.readFileSync("./media/myimage.png");
-    try {
-      client.post(
-        "media/upload",
-        {
-          media: imageData
-        },
-        function(error, media, response) {
-          if (error) {
-            console.log(error);
-          } else {
-            const status = {
-              status: "I tweeted from Node.js!",
-              media_ids: media.media_id_string
-            };
-            client.post("statuses/update", status, function(error, response) {
-              if (error) {
-                console.log(error);
-              } else {
-                debugger;
-                res.status(200).json({
-                  message: response.entities.media[0].display_url
-                });
-                // console.log("Media", response.entities.media[0].display_url);
-              }
-            });
-          }
+  // do stuff
+  const imageData = fs.readFileSync("./media/myimage.png");
+  try {
+    client.post(
+      "media/upload",
+      {
+        media: imageData
+      },
+      function(error, media, response) {
+        if (error) {
+          console.log(error);
+        } else {
+          const status = {
+            status: "I tweeted from Node.js!",
+            media_ids: media.media_id_string
+          };
+          client.post("statuses/update", status, function(error, response) {
+            if (error) {
+              console.log(error);
+            } else {
+              debugger;
+              res.status(200).json({
+                message: response.entities.media[0].display_url
+              });
+              // console.log("Media", response.entities.media[0].display_url);
+            }
+          });
         }
-      );
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-    deleteImage();
-  });
+      }
+    );
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+  deleteImage();
 });
 
 server.get("/img", (req, res) => {
